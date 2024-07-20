202d72fa381e8444986944d1ff07656f60861a15
MinhTri <119898661+akaJirt@users.noreply.github.com>
2024-07-21 05:41:53 +0700

    update some promo

--- src/api/models/PromotionsModel.js
+++ src/api/models/PromotionsModel.js
@@ -9,31 +9,26 @@ const promotionSchema = new mongoose.Schema(
       uppercase: true,
       required: [true, "A promotion must have a code!"],
     },
-
     description: {
       type: String,
       trim: true,
       required: [true, "A promotion must have a description!"],
     },
-
     discount: {
       type: Number,
       required: [true, "A promotion must have a discount!"],
     },
-
     discountType: {
       type: String,
       enum: ["fixed", "percentage", "maxPercentage"],
       required: [true, "A promotion must have a discount type!"],
     },
-
     maxDiscount: {
       type: Number,
       required: function () {
         return this.discountType === "maxPercentage";
       },
     },
-
     minOrderValue: {
       type: Number,
       required: function () {
@@ -44,12 +39,16 @@ const promotionSchema = new mongoose.Schema(
       type: Date,
       required: [true, "A promotion must have a start date!"],
     },
-
     endDate: {
       type: Date,
       required: [true, "A promotion must have an end date!"],
+      validate: {
+        validator: function (value) {
+          return value > this.startDate;
+        },
+        message: "End date must be after start date!",
+      },
     },
-
     isActive: {
       type: Boolean,
       default: true,
@@ -60,6 +59,26 @@ const promotionSchema = new mongoose.Schema(
   }
 );
 
-const Promotion = mongoose.model("promotions", promotionSchema);
+// Method to check if the promotion is active
+promotionSchema.methods.isActivePromotion = function () {
+  const currentDate = new Date();
+  return (
+    this.isActive &&
+    currentDate >= this.startDate &&
+    currentDate <= this.endDate
+  );
+};
+
+// Static method to find active promotions
+promotionSchema.statics.findActivePromotions = function () {
+  const currentDate = new Date();
+  return this.find({
+    isActive: true,
+    startDate: { $lte: currentDate },
+    endDate: { $gte: currentDate },
+  });
+};
+
+const Promotion = mongoose.model("Promotion", promotionSchema);
 
 module.exports = Promotion;
