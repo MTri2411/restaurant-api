f1f30166b908a78b1b17ceafeef6306fd3ec7c3f
MTri2411 <luongminhtri2411@gmail.com>
2025-02-12 02:34:45 +0700

    update firebase

--- server.js
+++ server.js
@@ -16,3 +16,9 @@ const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
